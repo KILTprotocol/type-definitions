b1ac0c86a65be21bba84853386ed51fb16902146
Albrecht <14820950+weichweich@users.noreply.github.com>
2021-09-15 16:10:54 +0200

    fix: add types from #19

--- src/types_23.ts
+++ src/types_23.ts
@@ -3,6 +3,28 @@ import { types21 } from "./types_21";
 
 export const types23: RegistryTypes = {
   ...types21,
+  MaxCollatorCandidates: undefined,
+  MinSelectedCandidates: undefined,
+  MinCollators: "u32",
+  MaxTopCandidates: "u32",
+
+  // Renamed collator to candidate since they are not always collators (most of them are candidates)
+  Collator: undefined,
+  CollatorStatus: undefined,
+  Candidate: {
+    id: "AccountId",
+    stake: "Balance",
+    delegators: "Vec<Stake>",
+    total: "Balance",
+    // renamed from state to status to be consistent
+    status: "CandidateStatus",
+  },
+  CandidateStatus: {
+    _enum: {
+      Active: "Null",
+      Leaving: "SessionIndex",
+    },
+  },
   DidAuthorizedCallOperation: {
     did: "DidIdentifierOf",
     txCounter: "u64",
