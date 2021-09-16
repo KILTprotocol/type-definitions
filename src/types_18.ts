ec26ed97c39fde11761c90989b197a7ee30d26a9
Albrecht <14820950+weichweich@users.noreply.github.com>
2021-09-16 14:09:57 +0200

    feat: split into files & add types for 23 (#20)
    * feat: split into files & add types for 23
    
    * fix: add types from #19
    
    * update version
    
    * spelling
    
    * 🧹🧹🧹🧹
    
    * remove did calls
    
    * export
    
    * smart export!

---
+++ src/types_18.ts
@@ -0,0 +1,27 @@
+import type { RegistryTypes } from "@polkadot/types/types";
+import { types17 } from "./types_17";
+
+export const types18: RegistryTypes = {
+  ...types17,
+  // Remove old DID types
+  DidCreationOperation: undefined,
+  DidUpdateOperation: undefined,
+  DidDeletionOperation: undefined,
+
+  // DID management update
+  DidCreationDetails: {
+    did: "DidIdentifierOf",
+    newKeyAgreementKeys: "BTreeSet<DidEncryptionKey>",
+    newAttestationKey: "Option<DidVerificationKey>",
+    newDelegationKey: "Option<DidVerificationKey>",
+    newEndpointUrl: "Option<Url>",
+  },
+  DidUpdateDetails: {
+    newAuthenticationKey: "Option<DidVerificationKey>",
+    newKeyAgreementKeys: "BTreeSet<DidEncryptionKey>",
+    attestationKeyUpdate: "DidVerificationKeyUpdateAction",
+    delegationKeyUpdate: "DidVerificationKeyUpdateAction",
+    publicKeysToRemove: "BTreeSet<KeyIdOf>",
+    newEndpointUrl: "Option<Url>",
+  },
+};
